ef0f4996cc5ffe2bc77426e9bed6c9e0af665d20
drigoni <davider1994@gmail.com>
2019-12-10 12:40:07 +0100

    Add _environments

---
+++ _environments/uninstall.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Create envs
+
+# analysis
+conda deactivate
+conda remove --name analysis --all
+conda remove --name gvae --all
+conda remove --name sdvae --all
+conda remove --name molgan --all
+conda remove --name organic --all
+conda remove --name jtvae --all
+conda remove --name cgvae --all
+conda remove --name rgvae --all
+
+
