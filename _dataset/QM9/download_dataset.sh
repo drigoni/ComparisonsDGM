40380e67341c30be09d141cd753ead8da744a7c8
Davide Rigoni <davider1994@gmail.com>
2020-09-16 12:36:02 +0200

    Update links

--- _dataset/QM9/download_dataset.sh
+++ _dataset/QM9/download_dataset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/gdb9.tar.gz 
 tar xvzf gdb9.tar.gz
-wget https://github.com/gablg1/ORGAN/raw/master/organ/NP_score.pkl.gz
-wget https://github.com/gablg1/ORGAN/raw/master/organ/SA_score.pkl.gz
+wget https://www.dropbox.com/s/yezw6s8srglhjqy/NP_score.pkl.gz?dl=0
+wget https://www.dropbox.com/s/b1uu207r2nhh3it/SA_score.pkl.gz?dl=0
